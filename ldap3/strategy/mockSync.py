cf4574858fd4532a068c8afab450231c1c077c70
Roxana Gherle <roxana.gherle@yahoo.com>
2016-04-12 18:14:48 +0000

    Enhance mockSync ladp3 strategy

--- ldap3/strategy/mockSync.py
+++ ldap3/strategy/mockSync.py
@@ -87,57 +87,16 @@ class MockSyncStrategy(SyncStrategy):
         self.can_stream = False
         self.receiving_mock = None
 
-    def sending(self, ldap_message):
-        pass
-
-    def _mock_search_response(self, request):
-        import pdb; pdb.set_trace()
-
-        # discover if it's a user or group query
-        search_base = request['baseObject']
-        search_base = re.sub(r'\s+', '', str(search_base))
-
-        request_filter = request['filter']
-        search_eq = request_filter['equalityMatch']
-
-        if search_eq is not None:
-            search_attr = search_eq['attributeDesc']
-            search_value = search_eq['assertionValue']
-
-        receiving_mock = []
-        if search_base == USER_TREE_DN:
-            if search_eq is not None and search_attr == 'uid':
-                receiving_mock.append(self._build_user_response(search_value))
-            elif search_eq is None:
-                receiving_mock.extend([self._build_user_response(user) for user in mock_users.iterkeys()])
-            else:
-                # FIXME: raise an exception
-                pass
-        elif search_base == GROUP_TREE_DN:
-            if search_eq is not None and search_attr == 'cn':
-                receiving_mock.append(self._build_group_response(search_value))
-            elif search_eq is None:
-                receiving_mock.extend([self._build_group_response(group) for group in mock_groups.iterkeys()])
-            else:
-                # FIXME: raise an exception
-                pass
-        elif USER_TREE_DN in search_base and search_base.startswith('uid='):
-            user_search = search_base.split(',')[0].split('=')[1]
-            if user_search in mock_users.keys():
-                receiving_mock.append(self._build_user_response(user_search))
-        receiving_mock.append(self._build_searchResDone_dict())
-        return receiving_mock
-
     def _build_user_response(self, user):
         user_dict_resp = mock_users[user]
-        #user_dict_resp['attributes'] = {str(k): str(v) for k, v in user_dict_resp['attributes']}
+        # TODO: convert raw_attributes to bytestrings
         user_dict_resp['raw_attributes'] = user_dict_resp['attributes']
         user_dict_resp['type'] = 'searchResEntry'
         return user_dict_resp
 
     def _build_group_response(self, group):
         group_dict_resp = mock_groups[group]
-        #group_dict_resp['attributes'] = {str(k): str(v) for k, v in group_dict_resp['attributes']}
+        # TODO: convert raw_attributes to bytestrings
         group_dict_resp['raw_attributes'] = group_dict_resp['attributes']
         group_dict_resp['type'] = 'searchResEntry'
         return group_dict_resp
@@ -162,6 +121,49 @@ class MockSyncStrategy(SyncStrategy):
         bind_dict_resp['type'] = 'bindResponse'
         return [bind_dict_resp]
 
+
+    def _mock_search_response(self, request):
+        import pdb
+        pdb.set_trace()
+        # discover if it's a user or group query
+        search_base = request['baseObject']
+        search_base = re.sub(r'\s+', '', str(search_base))
+
+        request_filter = request['filter']
+        search_eq = request_filter['equalityMatch']
+
+        if search_eq is not None:
+            search_attr = search_eq['attributeDesc']
+            search_value = search_eq['assertionValue']
+
+        receiving_mock = []
+        if search_base == USER_TREE_DN:
+            if search_eq is not None and search_attr == 'uid':
+                receiving_mock.append(self._build_user_response(search_value))
+            elif search_eq is None:
+                receiving_mock.extend(
+                    [self._build_user_response(user)
+                     for user in mock_users.iterkeys()])
+            else:
+                # FIXME: raise an exception
+                pass
+        elif search_base == GROUP_TREE_DN:
+            if search_eq is not None and search_attr == 'cn':
+                receiving_mock.append(self._build_group_response(search_value))
+            elif search_eq is None:
+                receiving_mock.extend(
+                    [self._build_group_response(group)
+                     for group in mock_groups.iterkeys()])
+            else:
+                # FIXME: raise an exception
+                pass
+        elif USER_TREE_DN in search_base and search_base.startswith('uid='):
+            user_search = search_base.split(',')[0].split('=')[1]
+            if user_search in mock_users.keys():
+                receiving_mock.append(self._build_user_response(user_search))
+        receiving_mock.append(self._build_searchResDone_dict())
+        return receiving_mock
+
     def send(self, message_type, request, controls=None):
         self.connection.listening = True
 
@@ -174,6 +176,9 @@ class MockSyncStrategy(SyncStrategy):
         return super(MockSyncStrategy, self).send(message_type,
                                                   request,
                                                   controls)
+
+    def sending(self, ldap_message):
+        pass
 
     def receiving(self):
         pass
